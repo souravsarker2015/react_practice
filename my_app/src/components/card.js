b7aa68e39faf639fc824c670b3d59cc291042bb6
sourov-sarker8251 <souravsarker2015@gmail.com>
2022-04-01 17:01:09 +0600

    react practice component ,app.js

---
+++ my_app/src/components/card.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const todoTitle = "Tito";
+const todoDescription = "Bangladesh, to the east of India on the Bay of Bengal, is a South Asian country marked by lush greenery and many\n" +
+    "        waterways. Its Padma (Ganges), Meghna and Jamuna rivers create fertile plains, and travel by boat is common. On\n" +
+    "        the southern coast, the Sundarbans, an enormous mangrove forest shared with Eastern India, is home to the royal\n" +
+    "        Bengal tiger. ";
+const date = new Date();
+const todo_date = date.getDate();
+const todo_month = date.getMonth();
+const todo_year = date.getFullYear();
+
+function Card() {
+    return <div className="card">
+        <h2 className="cardTitle">{todoTitle}</h2>
+        <p className="cardDesc">{todoDescription}</p>
+        <p className="cardFooter">{todo_date + "/" + todo_month + "/" + todo_year}</p>
+    </div>
+}
+
+export default Card
--- my_app/src/index.js
+++ my_app/src/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import './card'
+// import Card from "./components/card";
+import App from "./App";
 
-const todoTitle = "Tito";
-const todoDescription = "Bangladesh, to the east of India on the Bay of Bengal, is a South Asian country marked by lush greenery and many\n" +
-    "        waterways. Its Padma (Ganges), Meghna and Jamuna rivers create fertile plains, and travel by boat is common. On\n" +
-    "        the southern coast, the Sundarbans, an enormous mangrove forest shared with Eastern India, is home to the royal\n" +
-    "        Bengal tiger. ";
-const date=new Date();
-const todo_date=date.getDate();
-const todo_month=date.getMonth();
-const todo_year=date.getFullYear();
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
 // const headingStyle={
 //     backgroundColor:'purple',
 //     color:"red",
@@ -20,23 +24,6 @@ const todo_year=date.getFullYear();
 //     fontSize:"3rem",
 //     padding:"15px"
 // }
-ReactDOM.render(
-
-<div>
-    {/*<h1 style={headingStyle}>To do</h1>*/}
-    <h1 className="headingStyle largeTest">To do</h1>
-    <h2>{todoTitle}</h2>
-    <p>{todoDescription}</p>
-    <p>{todo_date +"/"+todo_month+"/"+todo_year}</p>
-</div>,
-    document.getElementById('root')
-)
-;
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
 
 // <React.StrictMode>
 //   <App />
